ab5d616248f6bbb637b7bf2c7300c9ab50b5bfc4
Felix Becker <felix.b@outlook.com>
2020-05-27 11:00:16 +0200

    ci: remove cmd/precise-code-intel (#90)

--- check_dependent.sh
+++ check_dependent.sh
@@ -10,10 +10,6 @@ git clone --depth 1 https://github.com/sourcegraph/sourcegraph "$CLONE_DIR"
 cd "$CLONE_DIR"
 echo "--- yarn"
 yarn --frozen-lockfile
-echo "--- yarn cmd/precise-code-intel"
-pushd cmd/precise-code-intel
-yarn --frozen-lockfile
-popd
 echo "--- yarn add"
 yarn add --dev "$CONFIG_DIR"
 yarn deduplicate
